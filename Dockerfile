494111f778409cd9d3aa8943679b946b8f3beb4a
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2018-10-21 03:21:24 +0900

    Use docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,15 @@
 FROM ruby:2.5.1-slim-stretch
 
-VOLUME /dev/shm
-
 ENV HOME=/chunithm-recorder
 WORKDIR $HOME
 
-RUN apt-get update && apt-get install -y unzip wget busybox-static curl gnupg2 && \
-    CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
-    wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/ && \
-    unzip ~/chromedriver_linux64.zip -d ~/ && \
-    rm ~/chromedriver_linux64.zip && \
-    chown root:root ~/chromedriver && \
-    chmod 755 ~/chromedriver && \
-    mv ~/chromedriver /usr/bin/chromedriver && \
-    sh -c 'wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -' && \
-    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && \
-    apt-get update && apt-get install -y google-chrome-stable
-
-RUN apt-get install -y build-essential patch ruby-dev zlib1g-dev liblzma-dev
+RUN apt-get update && apt-get install -y \
+            build-essential \
+            patch \
+            ruby-dev \
+            zlib1g-dev \
+            liblzma-dev \
+            busybox-static
 
 COPY Gemfile $HOME
 COPY Gemfile.lock $HOME
@@ -25,13 +17,10 @@ RUN bundle install
 
 COPY config $HOME/config
 COPY lib $HOME/lib
-COPY Rakefile $HOME
-COPY unicorn.conf $HOME
-COPY config.ru $HOME
+COPY bin $HOME/bin
 
-RUN mkdir -p $HOME/tmp/pids $HOME/log
 RUN mkdir -p /var/spool/cron/crontabs/
 ENV TZ=Asia/Tokyo
-RUN echo '* 1 * * * cd /chunithm-recorder && bundle exec rake record' > /var/spool/cron/crontabs/root
+RUN echo '10 0 * * * cd /chunithm-recorder && bundle exec ./bin/chunithm record --remote' > /var/spool/cron/crontabs/root
 
 CMD ["busybox", "crond", "-f"]
--- lib/chunithm_recorder.rb
+++ lib/chunithm_recorder.rb
@@ -5,47 +5,62 @@ require 'google/cloud/bigquery'
 require 'time'
 
 class ChunithmRecorder
-  def initialize
+  def initialize(dryrun: false, remote: false)
     ENV['TZ'] = 'Asia/Tokyo'
     Dotenv.load
-    @options = Selenium::WebDriver::Chrome::Options.new
-    @options.add_argument('--headless')
-    @options.add_argument('--no-sandbox')
+    @dryrun = dryrun
+    @remote = remote
   end
 
   def load(day = Time.now-60*60*24)
-    go_to_history
-    load_to_bq(fetch_chunithm_record(day), 'score')
+    initialize_webdriver
+    records = fetch_chunithm_record(day)
+    if @dryrun
+      p records
+      return
+    end
+    load_to_bq(records, 'score')
   end
 
-  def go_to_history
-    puts "-" * 100
-    @driver.quit if @driver
-    @driver = Selenium::WebDriver.for :chrome, options: @options
+  def initialize_webdriver
+    if @remote
+      chrome_capabilities = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => {args: ['--headless', '--no-sandbox']})
+      @driver = Selenium::WebDriver.for :remote, url: ENV['SELENIUM_URL'], desired_capabilities: chrome_capabilities
+    else
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--headless')
+      options.add_argument('--no-sandbox')
+      @driver = Selenium::WebDriver.for :chrome, options: options
+    end
+  end
+
+  def login_chunithm
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
     @driver.navigate.to 'https://chunithm-net.com/mobile/Index.html'
     @driver.find_element(:name, 'segaId').send_keys(ENV['CHUNITHM_SEGA_ID'])
     @driver.find_element(:name, 'password').send_keys(ENV['CHUNITHM_PASSWORD'])
     @driver.find_element(:xpath, '//div[contains(@class, "btn_login")]').click
 
+    wait.until {@driver.find_element(:xpath, '//div[contains(@class, "btn_select_aime")]').enabled?}
     @driver.find_element(:xpath, '//div[contains(@class, "btn_select_aime")]').click
-
-    @driver.find_element(:link_text, 'プレイ履歴').click
   end
 
   def fetch_chunithm_record(day)
+    login_chunithm
+    @driver.navigate.to 'https://chunithm-net.com/mobile/Playlog.html'
     records = []
     50.times do |i|
       retry_count = 0
       begin
+        puts @driver.current_url
         wait = Selenium::WebDriver::Wait.new(:timeout => 20)
-        wait.until { @driver.find_element(:xpath, %Q|//a[contains(@onclick, "JavaScript:pageMove('PlaylogDetail',#{i});")]|).enabled? }
+        wait.until { @driver.find_element(:xpath, %Q|//a[contains(@onclick, "JavaScript:pageMove('PlaylogDetail',#{i});")]|).displayed? }
         @driver.find_element(:xpath, %Q|//a[contains(@onclick, "JavaScript:pageMove('PlaylogDetail',#{i});")]|).click
-        date_element = @driver.find_element(:xpath, '//div[contains(@class, "box_inner01")]')
-        wait.until { date_element.enabled? }
-        puts date = date_element.text
+        wait.until { @driver.find_element(:xpath, '//div[contains(@class, "box_inner01")]').displayed? }
+        puts date = @driver.find_element(:xpath, '//div[contains(@class, "box_inner01")]').text
         return records if Time.parse(date) < day
         unless date.match?(/#{day.strftime("%Y-%m-%d")}/)
-          @driver.navigate.back
+          @driver.navigate.to 'https://chunithm-net.com/mobile/Playlog.html'
           next
         end
         puts title = @driver.find_element(:xpath, '//div[contains(@class, "play_musicdata_title")]').text
@@ -60,7 +75,7 @@ class ChunithmRecorder
         attack = @driver.find_element(:xpath, '//div[contains(@class, "play_musicdata_judgenumber text_attack")]').text
         miss = @driver.find_element(:xpath, '//div[contains(@class, "play_musicdata_judgenumber text_miss")]').text
         tap, hold, slide, air, flick = @driver.find_elements(:xpath, '//div[contains(@class, "play_musicdata_notesnumber")]').map(&:text).map(&:chop).map(&:to_f)
-        @driver.navigate.back
+        @driver.navigate.to 'https://chunithm-net.com/mobile/Playlog.html'
         records << {
           'title' => title,
           'score' => score,
@@ -82,7 +97,10 @@ class ChunithmRecorder
         retry_count += 1
         puts "retry #{retry_count} times"
         raise 'Failed to fetch data' if retry_count > 3
-        go_to_history
+        @driver.quit if @driver
+        initialize_webdriver
+        login_chunithm
+        @driver.navigate.to 'https://chunithm-net.com/mobile/Playlog.html'
         retry
       end
     end
@@ -111,19 +129,112 @@ class ChunithmRecorder
     end
   end
 
-  def get_master_data
+  def fetch_rate_values
     difficulty = { 'MAS' => 'master', 'EXP' => 'expert'}
 
-    driver = Selenium::WebDriver.for :chrome, options: @options
-    driver.navigate.to 'https://chuniviewer.net/ratevaluelist'
-    table = driver.find_element(:id, 'rate-list-table').find_element(:tag_name, 'tbody')
+    initialize_webdriver
+    @driver.navigate.to 'https://chuniviewer.net/ratevaluelist'
+    table = @driver.find_element(:id, 'rate-list-table').find_element(:tag_name, 'tbody')
     records = table.find_elements(:tag_name, 'tr').map do |tr|
       e = tr.find_elements(:tag_name, 'td').map(&:text)
       e[2] = difficulty[e[2]]
       e.last ? ['level', 'title', 'difficulty', 'rate_value'].zip(e).to_h : nil
     end
-    p records
-    load_to_bq(records.reject(&:nil?), 'master')
+    if @dryrun
+      p records
+    else
+      load_to_bq(records.reject(&:nil?), 'rate_values')
+    end
+  end
+
+  def fetch_notes
+    initialize_webdriver
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+
+    @driver.navigate.to 'https://chunithm.gamerch.com/CHUNITHM%20STAR%20PLUS%20%E6%A5%BD%E6%9B%B2%E4%B8%80%E8%A6%A7'
+
+    wait.until {@driver.find_element(:xpath, '//div[contains(@class, "star-midashi1")]').enabled?}
+    p genres = @driver.find_elements(:xpath, '//div[contains(@class, "star-midashi1")]').map(&:text)[0..6]
+    wait.until {@driver.find_element(:xpath, '//div[contains(@class, "t-line-img")]')}
+    p tracks_url = @driver
+               .find_element(:xpath, '//div[contains(@class, "t-line-img")]')
+               .find_elements(:tag_name, 'tbody')[0..6]
+               .map { |e| e.find_elements(:tag_name, 'tr')
+                        .map { |e| e.find_element(:tag_name, 'th')
+                                 .find_element(:tag_name, 'a').attribute('href')}
+    }
+    records = tracks_url.zip(genres).map { |urls,genre| urls.map { |url| fetch_track(url, genre)} }.flatten
+    if @dryrun
+      p records
+    else
+      load_to_bq(records, 'notes')
+    end
+
+  end
+
+  def fetch_track(url,genre)
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+    @driver.navigate.to url
+    wait.until {@driver.find_element(:xpath, '//span[contains(@id, "js_async_main_column_name")]').enabled?}
+    p title = @driver.find_element(:xpath, '//span[contains(@id, "js_async_main_column_name")]').text
+    wait.until {@driver.find_element(:tag_name, 'tbody').enabled?}
+    exp, mas = @driver.find_elements(:tag_name, 'tbody')
+                 .select { |e| e.find_element(:tag_name, 'tr').find_elements(:tag_name, 'th').map(&:text) == ['Lv', '総数', '内訳']}.first
+                 .find_elements(:tag_name, 'tr')[4..5].map { |tr| tr.find_elements(:tag_name, 'td').map(&:text).map(&:to_i) }
+    labels = ['all', 'tap', 'hold', 'slide', 'air', 'flick']
+    [
+      { 'title' => title, 'difficulty' => 'expert', 'genre' => genre}.merge(labels.zip(exp).to_h),
+      { 'title' => title, 'difficulty' => 'master', 'genre' => genre}.merge(labels.zip(mas).to_h)
+    ]
+  end
+
+  def fetch_record_history
+    initialize_webdriver
+    login_chunithm
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+    @driver.navigate.to 'https://chunithm-net.com/mobile/MusicGenre.html'
+    wait.until { @driver.find_element(:class_name, 'btn_master').enabled? }
+    @driver.find_element(:class_name, 'btn_master').click
+    wait.until {@driver.find_element(:class_name, 'music_title').enabled?}
+    onclick = @driver.find_elements(:class_name, 'musiclist_box')
+                .select { |e| e.find_elements(:class_name, 'play_musicdata_highscore').size > 0 }
+                .map { |e| e.find_element(:class_name, 'music_title').attribute('onclick')}
+    records = onclick.map do |e|
+      wait.until {@driver.find_element(:xpath, %Q|//div[contains(@onclick, "#{e}")]|).enabled?}
+      @driver.find_element(:xpath, %Q|//div[contains(@onclick, "#{e}")]|).click
+      wait.until { @driver.find_element(:class_name, 'play_musicdata_title') }
+      puts title = @driver.find_element(:class_name, 'play_musicdata_title').text
+      records = []
+      if @driver.find_elements(:class_name, 'bg_master').size > 0
+        master = @driver.find_element(:class_name, 'bg_master')
+        records << {
+          'title' => title,
+          'difficulty' => 'master',
+          'updated_at' => master.find_element(:class_name, 'musicdata_detail_date').text,
+          'high_score' => master.find_element(:class_name, 'text_b').text.gsub(/,/, '_').to_i,
+          'play_count' => master.find_element(:class_name, 'block_icon_text').find_elements(:tag_name, 'span').last.text
+        }
+      end
+
+      if @driver.find_elements(:class_name, 'bg_expert').size > 0
+        expert = @driver.find_element(:class_name, 'bg_expert')
+        records << {
+          'title' => title,
+          'difficulty' => 'expert',
+          'updated_at' => expert.find_element(:class_name, 'musicdata_detail_date').text,
+          'high_score' => expert.find_element(:class_name, 'text_b').text.gsub(/,/, '_').to_i,
+          'play_count' => expert.find_element(:class_name, 'block_icon_text').find_elements(:tag_name, 'span').last.text
+        }
+      end
+      @driver.navigate.back
+      records
+    end
+
+    if @dryrun
+      p records
+    else
+      load_to_bq(records.flatten, 'record_history')
+    end
   end
 end
 
